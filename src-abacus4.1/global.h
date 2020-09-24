b6cab5d682bca268c8942c9113f6f949955ccbac
Sora-dcliu <m18173401355@163.com>
2020-09-24 16:52:15 +0800

    new version 4.1

---
+++ src-abacus4.1/global.h
@@ -0,0 +1,26 @@
+#ifndef _GLOBAL_
+#define _GLOBAL_
+
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+
+#include "cell.h"
+
+// global variable
+extern int Row_cnt;   // number of Rows - M
+extern int Col_cnt;   // number of columns - N
+extern int Cell_cnt;  // number of cells
+
+extern vector<shared_ptr<cell>> CELLS;
+
+// global function
+void getInput(const string& inputFile);
+void Output(const string& outputFile);
+void WriteGds(const string& filename);
+long long getTotalCost();
+void checkLegal();
+
+#endif
