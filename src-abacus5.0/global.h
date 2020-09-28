78bc6b57e938027978fba293e31575e508b1f87c
Sora-dcliu <m18173401355@163.com>
2020-09-28 09:38:39 +0800

    create v5.0

---
+++ src-abacus5.0/global.h
@@ -0,0 +1,29 @@
+#ifndef _GLOBAL_
+#define _GLOBAL_
+
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "cell.h"
+#include "limits.h"
+
+// global variable
+extern int Row_cnt;   // number of Rows - M
+extern int Col_cnt;   // number of columns - N
+extern int Cell_cnt;  // number of cells
+
+extern vector<shared_ptr<cell>> CELLS;
+
+// global function
+void getInput(const string& inputFile);
+void Output(const string& outputFile);
+void WriteGds(const string& filename);
+long long getTotalCost();
+void checkLegal();
+
+#endif
