702bde6563587f42fd98616701198e343a7f7d19
Sora-dcliu <m18173401355@163.com>
2020-11-12 10:37:38 +0800

    update

--- src-abacus5.0/bipartite.cpp
+++ src-abacus5.0/bipartite.cpp
@@ -32,17 +32,23 @@ void BGM::doBipartiteGraphMatch() {
       set<shared_ptr<cell>> insts;
       this->dfs(x, y, insts);
       if (insts.size() > 1) {
-        int lx = max_x_;
-        int ly = max_y_;
-        int ux = 0;
-        int uy = 0;
+        int lx_bbx = max_x_;
+        int ly_bbx = max_y_;
+        int ux_bbx = 0;
+        int uy_bbx = 0;
         for (auto& inst : insts) {
-          lx = min(lx, inst->lx() / 8);
-          ux = max(ux, inst->lx() / 8);
-          ly = min(ly, inst->ly());
-          uy = max(uy, inst->ly());
+          lx_bbx = min(lx_bbx, inst->lx() / 8);
+          ux_bbx = max(ux_bbx, inst->lx() / 8);
+          ly_bbx = min(ly_bbx, inst->ly());
+          uy_bbx = max(uy_bbx, inst->ly());
         }
-        this->KM_match(lx, ly, ux, uy, insts);
+        this->lx = lx_bbx;
+        this->ly = ly_bbx;
+        this->ux = ux_bbx;
+        this->uy = uy_bbx;
+        cout << x << " " << y << endl;
+        this->cleanKM();
+        this->Match(insts);
       }
     }
   }
@@ -51,7 +57,7 @@ void BGM::doBipartiteGraphMatch() {
 // Depth-first search clustering
 void BGM::dfs(int x, int y, set<shared_ptr<cell>>& insts) {
   auto& bin = Grid[x][y];
-  if (bin.isStd() && !bin.visited()) {
+  if ((bin.isStd() || bin.isMacro()) && !bin.visited()) {
     insts.insert(bin.Inst());
     bin.mark();
     if (x - 1 >= 0) dfs(x - 1, y, insts);
@@ -62,13 +68,224 @@ void BGM::dfs(int x, int y, set<shared_ptr<cell>>& insts) {
 }
 
 // KM algorithm
-void BGM::KM_match(int lx, int ly, int ux, int uy, set<shared_ptr<cell>>& insts) {
+void BGM::Match(set<shared_ptr<cell>>& insts) {
   // init
-  vector<shared_ptr<cell>> vec_insts;
-  for (auto& inst : insts) vec_insts.push_back(inst);
   int width = ux - lx + 1;
   int height = uy - ly + 1;
-  cnt_inst = insts.size();
+  vector<shared_ptr<cell>>().swap(vec_insts);
+  vector<shared_ptr<cell>>().swap(macros);
+  for (auto& inst : insts) {
+    if (inst->isMacro())
+      macros.push_back(inst);
+    else
+      vec_insts.push_back(inst);
+  }
+  buildMap();
+  long long bestCost = KM();
+  this->setLocation();
+  if (macros.size() == 0) {
+    // update locations
+    cleanKM();
+    return;
+  }
+
+  sort(macros.begin(), macros.end(), [](shared_ptr<cell> inst1, shared_ptr<cell> inst2) {
+    return inst1->height() > inst2->height();
+  });
+  for (int idx = 0; idx < macros.size(); idx++) {
+    auto& macro = macros[idx];
+    // left->right
+    {
+      int x = macro->lx() / 8 - 1;
+      bool move = false;
+      if (x >= lx)
+        for (int y = macro->ly(); y < macro->uy(); y++) {
+          auto& inst = this->Grid[x][y].Inst();
+          if (find(vec_insts.begin(), vec_insts.end(), inst) != vec_insts.end() &&
+              inst->oldlx() > macro->lx()) {
+            move = true;
+            break;
+          }
+        }
+      if (move) {
+        for (x = macro->lx() / 8 + 1; x <= ux; x++) {
+          long long cost =
+              this->MoveMacro(idx, 3) + macro->getCost(x * 8, macro->ly()) - macro->getCost();
+          if (cost < bestCost) {
+            bestCost = cost;
+            this->setLocation();
+            macro->setLoc(x * 8, macro->ly());
+            this->cleanKM();
+          } else {
+            for (int y = macro->ly(); y < macro->uy(); y++) {
+              this->Grid[x - 1][y].setInst(macro);
+              this->Grid[x][y].deleteInst();
+            }
+            for (auto& inst : vec_insts) this->Grid[inst->lx() / 8][inst->ly()].setInst(inst);
+            break;
+          }
+        }
+      }
+      // right->left
+      {
+        int x = macro->ux() / 8;
+        bool move = false;
+        if (x <= ux)
+          for (int y = macro->ly(); y < macro->uy(); y++) {
+            auto& inst = this->Grid[x][y].Inst();
+            if (find(vec_insts.begin(), vec_insts.end(), inst) != vec_insts.end() &&
+                inst->oldlx() < macro->ux() - 4) {
+              move = true;
+              break;
+            }
+          }
+        if (move) {
+          for (x = macro->lx() / 8 - 1; x >= lx; x--) {
+            long long cost =
+                this->MoveMacro(idx, 4) + macro->getCost(x * 8, macro->ly()) - macro->getCost();
+            if (cost < bestCost) {
+              bestCost = cost;
+              this->setLocation();
+              macro->setLoc(x * 8, macro->ly());
+              this->cleanKM();
+            } else {
+              for (int y = macro->ly(); y < macro->uy(); y++) {
+                this->Grid[x + 1][y].setInst(macro);
+                this->Grid[x][y].deleteInst();
+              }
+              for (auto& inst : vec_insts) this->Grid[inst->lx() / 8][inst->ly()].setInst(inst);
+              break;
+            }
+          }
+        }
+        // top->down
+        {
+          auto& inst = this->Grid[macro->lx() / 8][macro->uy()].Inst();
+          if (find(vec_insts.begin(), vec_insts.end(), inst) != vec_insts.end() &&
+              inst->oldly() < macro->uy()) {
+            if (!this->Grid[macro->lx() / 8][macro->ly() - 1].isMacro())
+              for (int y = macro->ly() - 1; y >= ly; y--) {
+                long long cost =
+                    this->MoveMacro(idx, 1) + macro->getCost(macro->lx(), y) - macro->getCost();
+                if (cost < bestCost) {
+                  bestCost = cost;
+                  this->setLocation();
+                  macro->setLoc(macro->lx(), y);
+                  this->cleanKM();
+                } else {
+                  this->Grid[macro->lx() / 8][macro->ly() - 1].deleteInst();
+                  this->Grid[macro->lx() / 8][macro->uy() - 1].setInst(macro);
+                  for (auto& inst : vec_insts) this->Grid[inst->lx() / 8][inst->ly()].setInst(inst);
+                  break;
+                }
+              }
+          }
+        }
+        // down->top
+        {
+          auto& inst = this->Grid[macro->lx() / 8][macro->ly() - 1].Inst();
+          if (find(vec_insts.begin(), vec_insts.end(), inst) != vec_insts.end() &&
+              inst->oldly() > macro->ly()) {
+            if (!this->Grid[macro->lx() / 8][macro->uy()].isMacro())
+              for (int y = macro->ly() + 1; y <= uy - macro->height(); y++) {
+                long long cost =
+                    this->MoveMacro(idx, 2) + macro->getCost(macro->lx(), y) - macro->getCost();
+                if (cost < bestCost) {
+                  bestCost = cost;
+                  this->setLocation();
+                  macro->setLoc(macro->lx(), y);
+                  this->cleanKM();
+                } else {
+                  this->Grid[macro->lx() / 8][macro->uy()].deleteInst();
+                  this->Grid[macro->lx() / 8][macro->ly()].setInst(macro);
+                  for (auto& inst : vec_insts) this->Grid[inst->lx() / 8][inst->ly()].setInst(inst);
+                  break;
+                }
+              }
+          }
+        }
+      }
+    }
+  }
+}
+
+long long BGM::MoveMacro(int idx, int type) {
+  auto& macro = macros[idx];
+  // 1-top->down 2-down->top 3-left->right 4-right->left
+  if (type == 1) {
+    auto& b1 = this->Grid[macro->lx() / 8][macro->ly() - 1];
+    auto& b2 = this->Grid[macro->lx() / 8][macro->uy() - 1];
+    b1.setInst(macro);
+    b2.deleteInst();
+
+  } else if (type == 2) {
+    auto& b1 = this->Grid[macro->lx() / 8][macro->ly()];
+    auto& b2 = this->Grid[macro->lx() / 8][macro->uy()];
+
+    b2.setInst(macro);
+    b1.deleteInst();
+  } else if (type == 3 || type == 4) {
+    int x = macro->lx() / 8;
+    int newx = (type == 3) ? (x + 1) : (x - 1);
+    for (int y = macro->ly(); y < macro->uy(); y++) {
+      this->Grid[x][y].deleteInst();
+      this->Grid[newx][y].setInst(macro);
+    }
+  }
+
+  this->buildMap();
+  return this->KM();
+}
+
+long long BGM::KM() {
+  // do KM
+  for (int i = 0; i < cnt_inst; i++) {
+    while (1) {
+      minz = LLONG_MAX;
+      fill(vis_inst.begin(), vis_inst.end(), false);
+      fill(vis_bin.begin(), vis_bin.end(), false);
+
+      if (findPath(i)) break;  // It's matched correctly
+      // Not matched, update top value
+      for (int j = 0; j < cnt_inst; j++)
+        if (vis_inst[j]) w_inst[j] += minz;
+
+      for (int j = 0; j < cnt_bin; j++)
+        if (vis_bin[j]) w_bin[j] -= minz;
+    }
+  }
+  long long ans = 0;
+  for (int i = 0; i < cnt_inst; i++) {
+    ans += w_inst[i] + w_bin[c_inst[i]];
+  }
+  return ans;
+}
+
+bool BGM::findPath(int u) {
+  vis_inst[u] = true;  // Join the augmented path
+  for (int v = 0; v < cnt_bin; v++) {
+    if (!vis_bin[v] && Map[u][v] != LLONG_MAX) {
+      // The bin point has not added into an augmented path, and there is a path
+      long long t = w_inst[u] + w_bin[v] - Map[u][v];
+      if (t == 0) {
+        vis_bin[v] = true;
+        if (c_bin[v] == -1 || findPath(c_bin[v])) {
+          c_inst[u] = v;
+          c_bin[v] = u;
+          return true;
+        }
+      } else if (t < 0) {
+        minz = min(minz, -t);
+      }
+    }
+  }
+  return false;
+}
+
+void BGM::initKM() {
+  int width = ux - lx + 1;
+  int height = uy - ly + 1;
+  cnt_inst = vec_insts.size();
   cnt_bin = width * height;
 
   w_inst.reserve(cnt_inst);
@@ -91,14 +308,18 @@ void BGM::KM_match(int lx, int ly, int ux, int uy, set<shared_ptr<cell>>& insts)
   vis_bin.reserve(cnt_bin);
   vis_inst.resize(cnt_inst);
   vis_bin.resize(cnt_bin);
+}
 
+void BGM::buildMap() {
+  this->initKM();
+  int height = uy - ly + 1;
   for (int i = 0; i < cnt_inst; i++) {
     vector<long long> edge(cnt_bin);
     for (int j = 0; j < cnt_bin; j++) {
       int x = lx + j / height;
       int y = ly + j % height;
       auto& bin = this->Grid[x][y];
-      if (bin.isMacro() || insts.find(bin.Inst()) == insts.end()) {
+      if (find(vec_insts.begin(), vec_insts.end(), bin.Inst()) == vec_insts.end()) {
         edge[j] = LLONG_MAX;
       } else {
         edge[j] = bin.getCost(vec_insts[i]);
@@ -107,24 +328,10 @@ void BGM::KM_match(int lx, int ly, int ux, int uy, set<shared_ptr<cell>>& insts)
     }
     Map.push_back(edge);
   }
+}
 
-  // do KM
-  for (int i = 0; i < cnt_inst; i++) {
-    while (1) {
-      minz = LLONG_MAX;
-      fill(vis_inst.begin(), vis_inst.end(), false);
-      fill(vis_bin.begin(), vis_bin.end(), false);
-
-      if (findPath(i)) break;  // It's matched correctly
-      // Not matched, update top value
-      for (int j = 0; j < cnt_inst; j++)
-        if (vis_inst[j]) w_inst[j] += minz;
-
-      for (int j = 0; j < cnt_bin; j++)
-        if (vis_bin[j]) w_bin[j] -= minz;
-    }
-  }
-  // update location
+void BGM::setLocation() {
+  int height = uy - ly + 1;
   for (int i = 0; i < cnt_bin; i++) {
     int x = lx + i / height;
     int y = ly + i % height;
@@ -134,31 +341,10 @@ void BGM::KM_match(int lx, int ly, int ux, int uy, set<shared_ptr<cell>>& insts)
       auto& inst = vec_insts[c_bin[i]];
       bin.setInst(inst);
       inst->setLoc(x * 8, y);
-    } else if (insts.find(bin.Inst()) != insts.end())
+    } else if (find(vec_insts.begin(), vec_insts.end(), bin.Inst()) != vec_insts.end()) {
       bin.deleteInst();
-  }
-  cleanKM();
-}
-
-bool BGM::findPath(int u) {
-  vis_inst[u] = true;  // Join the augmented path
-  for (int v = 0; v < cnt_bin; v++) {
-    if (!vis_bin[v] && Map[u][v] != LLONG_MAX) {
-      // The bin point has not added into an augmented path, and there is a path
-      long long t = w_inst[u] + w_bin[v] - Map[u][v];
-      if (t == 0) {
-        vis_bin[v] = true;
-        if (c_bin[v] == -1 || findPath(c_bin[v])) {
-          c_inst[u] = v;
-          c_bin[v] = u;
-          return true;
-        }
-      } else if (t < 0) {
-        minz = min(minz, -t);
-      }
     }
   }
-  return false;
 }
 
 void BGM::cleanKM() {
--- src-abacus5.0/bipartite.h
+++ src-abacus5.0/bipartite.h
@@ -32,7 +32,7 @@ class Bin {
   }
 
   inline void mark() { visited_ = true; };
-  inline void unmark() {visited_ = false;};
+  inline void unmark() { visited_ = false; };
   inline bool visited() { return visited_; };
 
  private:
@@ -49,21 +49,29 @@ class BGM {
 
   // For KM algorithm
   void dfs(int x, int y, set<shared_ptr<cell>>& insts);
-  void KM_match(int lx, int ly, int ux, int uy, set<shared_ptr<cell>>& insts);
+  void Match(set<shared_ptr<cell>>& insts);
+  long long MoveMacro(int idx, int type);  // 1-top->down 2-down->top 3-left->right 4-right->left
+  long long KM();
   bool findPath(int u);
 
+  void initKM();
+  void buildMap();
+  void setLocation();
   void cleanKM();
 
  private:
   int max_x_, max_y_;
   vector<vector<Bin>> Grid;
   // parameters for KM algorithm
-  int cnt_inst, cnt_bin;            // The number of points
-  vector<long long> w_inst, w_bin;  // The top value of each point
-  vector<int> c_inst, c_bin;        // The points that each point matches
-  vector<bool> vis_inst, vis_bin;   // Whether each point enters the augmented path
-  vector<vector<long long>> Map;    // Edge weights
-  long long minz;                   // The smallest difference
+  int lx, ly, ux, uy;                  // The coordinate of bbx
+  int cnt_inst, cnt_bin;               // The number of points
+  vector<shared_ptr<cell>> macros;     // The list of macros
+  vector<shared_ptr<cell>> vec_insts;  // The list of std_cells
+  vector<long long> w_inst, w_bin;     // The top value of each point
+  vector<int> c_inst, c_bin;           // The points that each point matches
+  vector<bool> vis_inst, vis_bin;      // Whether each point enters the augmented path
+  vector<vector<long long>> Map;       // Edge weights
+  long long minz;                      // The smallest difference
 };
 
 #endif
